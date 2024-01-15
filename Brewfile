cc6b70dbbce70d97fbf6b6d7cf1e9829fb6ca99f
Mattias Berglund <mattias.k.berglund@gmail.com>
2024-01-15 08:02:52 +0100

    some more config

--- Brewfile
+++ Brewfile
@@ -1,8 +1,12 @@
 brew "nvim"
 brew "tmux"
+brew "gnu-getopt"
+brew "node"
 
 cask "slack"
 # cask "google-drive"
 cask "keepassxc"
 cask "visual-studio-code"
 cask "spotify"
+cask "arc"
+cask "obsidian"
---
+++ astronvim/user/init.lua
@@ -0,0 +1,29 @@
+return {
+  plugins = {
+    { "akinsho/toggleterm.nvim", opts = { direction = "horizontal" } },
+    { "AstroNvim/astrocommunity", {
+          { import = "astrocommunity.colorscheme.nord-nvim" }
+    }},
+    {
+      "nvim-telescope/telescope.nvim",
+      opts = { defaults = { file_ignore_patterns = { "node_modules/*", "build/*", "*.svg" } } },
+    },
+  },
+  mappings = {
+    n = {
+      [".b"] = {
+        function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+        desc = "previous buffer",
+      },
+      [",b"] = {
+        function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+        desc = "next buffer",
+      },
+      ["<leader>tt"] = { function() require("toggleterm").toggle(vim.v.count) end, desc = "toggle term" },
+    },
+    t = {
+      ["<Esc>"] = { "<C-\\><C-n>", desc = "escape terminal" },
+    },
+  },
+  colorscheme = "nord"
+}
