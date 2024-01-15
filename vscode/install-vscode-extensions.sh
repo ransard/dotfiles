d545afe6d2f16927233b09de05b0d02677edf194
Mattias Berglund <mattias.k.berglund@gmail.com>
2024-01-15 14:27:16 +0100

    vscode settings + mac

---
+++ vscode/install-vscode-extensions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pkglist=(
+astro-build.astro-vscode
+bierner.markdown-mermaid
+bpruitt-goddard.mermaid-markdown-syntax-highlighting
+dbaeumer.vscode-eslint
+esbenp.prettier-vscode
+GitHub.copilot
+GitHub.copilot-chat
+ms-azuretools.vscode-azureappservice
+ms-azuretools.vscode-azureresourcegroups
+ms-vscode-remote.remote-wsl
+ms-vscode.azure-account
+Prisma.prisma
+svelte.svelte-vscode
+vscodevim.vim
+)
+
+for i in ${pkglist[@]}; do
+	code --install-extension $i
+done
